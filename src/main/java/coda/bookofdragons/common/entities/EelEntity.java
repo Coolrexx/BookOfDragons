e6e6b78376f6dbff78a939abce67a658834909ec
Coda <cody.koelliker@outlook.com>
2021-11-20 05:06:24 -0700

    make eels not go on the bottom of the seafloor, update 3 item textures

--- src/main/java/coda/bookofdragons/common/entities/EelEntity.java
+++ src/main/java/coda/bookofdragons/common/entities/EelEntity.java
@@ -9,8 +9,6 @@ import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
@@ -19,7 +17,6 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.control.MoveControl;
 import net.minecraft.world.entity.ai.control.SmoothSwimmingLookControl;
 import net.minecraft.world.entity.ai.control.SmoothSwimmingMoveControl;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
@@ -34,6 +31,7 @@ import net.minecraft.world.entity.animal.WaterAnimal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.pathfinder.AmphibiousNodeEvaluator;
 import net.minecraft.world.level.pathfinder.PathFinder;
 import net.minecraft.world.phys.HitResult;
@@ -55,8 +53,8 @@ public class EelEntity extends WaterAnimal implements IAnimatable, IAnimationTic
 
     public EelEntity(EntityType<? extends WaterAnimal> type, Level world) {
         super(type, world);
-        this.moveControl = new SmoothSwimmingMoveControl(this, 85, 40, 0.02F, 0.1F, true);
-        this.lookControl = new SmoothSwimmingLookControl(this, 40);
+        this.moveControl = new SmoothSwimmingMoveControl(this, 15, 10, 0.02F, 0.1F, true);
+        this.lookControl = new SmoothSwimmingLookControl(this, 10);
     }
 
     protected void registerGoals() {
@@ -70,6 +68,9 @@ public class EelEntity extends WaterAnimal implements IAnimatable, IAnimationTic
 
     public void travel(Vec3 p_27490_) {
         if (this.isEffectiveAi() && this.isInWater()) {
+            if (!level.getBlockState(blockPosition().below()).is(Blocks.WATER)) {
+                this.setDeltaMovement(this.getDeltaMovement().add(0, 0.015, 0));
+            }
             this.moveRelative(0.01F, p_27490_);
             this.move(MoverType.SELF, this.getDeltaMovement());
             this.setDeltaMovement(this.getDeltaMovement().scale(1.2D));
