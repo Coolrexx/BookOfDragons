4355b923f05fb5ebd5347968cf8412e9ccdd1fcb
Cody <cody.koelliker@outlook.com>
2021-11-23 01:01:55 -0700

    redo chest implementation

--- src/main/java/coda/bookofdragons/common/entities/util/AbstractRideableDragonEntity.java
+++ src/main/java/coda/bookofdragons/common/entities/util/AbstractRideableDragonEntity.java
@@ -2,21 +2,36 @@ package coda.bookofdragons.common.entities.util;
 
 import coda.bookofdragons.client.ClientEvents;
 import coda.bookofdragons.common.entities.util.goal.FollowDriverGoal;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.*;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.control.FlyingMoveControl;
+import net.minecraft.world.entity.animal.horse.AbstractChestedHorse;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ChestMenu;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.Nullable;
 
-public abstract class AbstractRideableDragonEntity extends AbstractFlyingDragonEntity {
+public abstract class AbstractRideableDragonEntity extends AbstractFlyingDragonEntity implements Saddleable, ContainerListener, MenuProvider {
+    private static final EntityDataAccessor<Boolean> CHESTED = SynchedEntityData.defineId(AbstractChestedHorse.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Boolean> SADDLED = SynchedEntityData.defineId(AbstractChestedHorse.class, EntityDataSerializers.BOOLEAN);
     public Entity previousDriver = null;
+    protected SimpleContainer inventory;
 
     public AbstractRideableDragonEntity(EntityType<? extends AbstractRideableDragonEntity> type, Level world) {
         super(type, world);
@@ -27,6 +42,13 @@ public abstract class AbstractRideableDragonEntity extends AbstractFlyingDragonE
     protected void registerGoals() {
         super.registerGoals();
         this.goalSelector.addGoal(3, new FollowDriverGoal(this));
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(CHESTED, false);
+        this.entityData.define(SADDLED  , false);
     }
 
     protected void removePassenger(Entity passenger) {
@@ -88,12 +110,206 @@ public abstract class AbstractRideableDragonEntity extends AbstractFlyingDragonE
         }
     }
 
+    public boolean hasChest() {
+        return this.entityData.get(CHESTED);
+    }
+
+    public void setChest(boolean p_30505_) {
+        this.entityData.set(CHESTED, p_30505_);
+    }
+
     @Override
+    public boolean isSaddleable() {
+        return true;
+    }
+
+    @Override
+    public void equipSaddle(@Nullable SoundSource p_21748_) {
+        this.inventory.setItem(0, new ItemStack(Items.SADDLE));
+        if (p_21748_ != null) {
+            this.level.playSound((Player)null, this, SoundEvents.HORSE_SADDLE, p_21748_, 0.5F, 1.0F);
+        }
+    }
+
+    @Override
+    public boolean isSaddled() {
+        return this.entityData.get(SADDLED);
+    }
+
+    public void setSaddled(boolean p_30505_) {
+        this.entityData.set(SADDLED, p_30505_);
+    }
+
+    protected void dropEquipment() {
+        super.dropEquipment();
+        if (this.hasChest()) {
+            if (!this.level.isClientSide) {
+                this.spawnAtLocation(Blocks.CHEST);
+            }
+
+            this.setChest(false);
+        }
+
+    }
+
+    public void addAdditionalSaveData(CompoundTag nbt) {
+        super.addAdditionalSaveData(nbt);
+        nbt.putBoolean("ChestedHorse", this.hasChest());
+        if (this.hasChest()) {
+            ListTag listtag = new ListTag();
+
+            for(int i = 2; i < this.inventory.getContainerSize(); ++i) {
+                ItemStack itemstack = this.inventory.getItem(i);
+                if (!itemstack.isEmpty()) {
+                    CompoundTag compoundtag = new CompoundTag();
+                    compoundtag.putByte("Slot", (byte)i);
+                    itemstack.save(compoundtag);
+                    listtag.add(compoundtag);
+                }
+            }
+
+            nbt.put("Items", listtag);
+        }
+
+        if (!this.inventory.getItem(0).isEmpty()) {
+            nbt.put("SaddleItem", this.inventory.getItem(0).save(new CompoundTag()));
+        }
+    }
+
+    public void readAdditionalSaveData(CompoundTag nbt) {
+        super.readAdditionalSaveData(nbt);
+        this.setChest(nbt.getBoolean("ChestedHorse"));
+        this.createInventory();
+        if (this.hasChest()) {
+            ListTag listtag = nbt.getList("Items", 10);
+
+            for(int i = 0; i < listtag.size(); ++i) {
+                CompoundTag compoundtag = listtag.getCompound(i);
+                int j = compoundtag.getByte("Slot") & 255;
+                if (j >= 2 && j < this.inventory.getContainerSize()) {
+                    this.inventory.setItem(j, ItemStack.of(compoundtag));
+                }
+            }
+        }
+
+        if (nbt.contains("SaddleItem", 10)) {
+            ItemStack itemstack = ItemStack.of(nbt.getCompound("SaddleItem"));
+            if (itemstack.is(Items.SADDLE)) {
+                this.inventory.setItem(0, itemstack);
+            }
+        }
+
+        this.updateContainerEquipment();
+    }
+
+    protected void createInventory() {
+        SimpleContainer simplecontainer = this.inventory;
+        this.inventory = new SimpleContainer(this.getInventorySize());
+        if (simplecontainer != null) {
+            simplecontainer.removeListener(this);
+            int i = Math.min(simplecontainer.getContainerSize(), this.inventory.getContainerSize());
+
+            for(int j = 0; j < i; ++j) {
+                ItemStack itemstack = simplecontainer.getItem(j);
+                if (!itemstack.isEmpty()) {
+                    this.inventory.setItem(j, itemstack.copy());
+                }
+            }
+        }
+
+        this.inventory.addListener(this);
+        this.updateContainerEquipment();
+        this.itemHandler = net.minecraftforge.common.util.LazyOptional.of(() -> new net.minecraftforge.items.wrapper.InvWrapper(this.inventory));
+    }
+
+    protected int getInventorySize() {
+        return this.hasChest() ? 18 : 3;
+    }
+
+    protected void updateContainerEquipment() {
+        if (!this.level.isClientSide) {
+            this.setSaddled(!this.inventory.getItem(0).isEmpty());
+        }
+    }
+
+    private net.minecraftforge.common.util.LazyOptional<?> itemHandler = null;
+
+    @Override
+    public <T> net.minecraftforge.common.util.LazyOptional<T> getCapability(net.minecraftforge.common.capabilities.Capability<T> capability, @Nullable net.minecraft.core.Direction facing) {
+        if (this.isAlive() && capability == net.minecraftforge.items.CapabilityItemHandler.ITEM_HANDLER_CAPABILITY && itemHandler != null)
+            return itemHandler.cast();
+        return super.getCapability(capability, facing);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        if (itemHandler != null) {
+            net.minecraftforge.common.util.LazyOptional<?> oldHandler = itemHandler;
+            itemHandler = null;
+            oldHandler.invalidate();
+        }
+    }
+
     public InteractionResult mobInteract(Player player, InteractionHand hand) {
-        if (player.getItemInHand(hand).is(Items.STICK)) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        if (!this.isBaby()) {
+            if (/*this.isTamed() && */player.isSecondaryUseActive()) {
+                player.openMenu(this);
+                return InteractionResult.sidedSuccess(this.level.isClientSide);
+            }
+
+            if (this.isVehicle()) {
+                return super.mobInteract(player, hand);
+            }
+        }
+
+        if (!itemstack.isEmpty()) {
+            if (!this.hasChest() && itemstack.is(Blocks.CHEST.asItem())) {
+                this.setChest(true);
+                this.playChestEquipsSound();
+                if (!player.getAbilities().instabuild) {
+                    itemstack.shrink(1);
+                }
+
+                this.createInventory();
+                return InteractionResult.sidedSuccess(this.level.isClientSide);
+            }
+
+            if (!this.isBaby() && !this.isSaddled() && itemstack.is(Items.SADDLE)) {
+                player.openMenu(this);
+                return InteractionResult.sidedSuccess(this.level.isClientSide);
+            }
+        }
+
+        if (this.isBaby()) {
+            return super.mobInteract(player, hand);
+        } else {
             player.startRiding(this);
             this.navigation.stop();
+            return InteractionResult.sidedSuccess(this.level.isClientSide);
         }
-        return super.mobInteract(player, hand);
+    }
+
+    protected void playChestEquipsSound() {
+        this.playSound(SoundEvents.DONKEY_CHEST, 1.0F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
+    }
+
+    @Nullable
+    @Override
+    public AbstractContainerMenu createMenu(int p_createMenu_1_, Inventory p_createMenu_2_, Player p_createMenu_3_) {
+        if (inventory == null) {
+            return null;
+        }
+        return inventory.getContainerSize() < 54 ? ChestMenu.threeRows(p_createMenu_1_, p_createMenu_2_, inventory) : ChestMenu.sixRows(p_createMenu_1_, p_createMenu_2_, inventory);
+    }
+
+    @Override
+    public void containerChanged(Container p_18983_) {
+        boolean flag = this.isSaddled();
+        this.updateContainerEquipment();
+        if (this.tickCount > 20 && !flag && this.isSaddled()) {
+            this.playSound(SoundEvents.HORSE_SADDLE, 0.5F, 1.0F);
+        }
     }
 }
